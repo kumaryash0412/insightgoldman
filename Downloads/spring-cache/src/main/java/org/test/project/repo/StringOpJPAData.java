405632d2bd8345c00c1b5cd55efe2d86cc0090c7
kumaryash0412 <62483572+kumaryash0412@users.noreply.github.com>
2021-03-25 12:32:51 -0400

    Delete Downloads/spring-cache directory

--- Downloads/spring-cache/src/main/java/org/test/project/repo/StringOpJPAData.java
+++
@@ -1,7 +0,0 @@
-package org.test.project.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.test.project.domain.StringOp;
-
-public interface StringOpJPAData extends CrudRepository<StringOp,String> {
-}
